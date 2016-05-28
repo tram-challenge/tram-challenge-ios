5deacf13dd06aaad964a578f7e3a5746b03c4c62
Stephen Sykes <sdsykes@gmail.com>
2016-05-28 13:19:09 +0300

    Tram stops model

---
+++ TramChallenge/TCTramStop.h
@@ -0,0 +1,22 @@
+//
+//  TCTramStop.h
+//  TramChallenge
+//
+//  Created by Stephen Sykes on 28/05/16.
+//  Copyright © 2016 Switchstep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface TCTramStop : NSObject
+
+@property (nonatomic) NSString *id;
+@property (nonatomic) CLLocationCoordinate2D coord;
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSArray<NSString *> *routes;
+@property (nonatomic) NSArray<NSDictionary<NSString *, NSString *> *> *links;
+@property (nonatomic) NSArray<NSString *> *hsl_ids;
+
+@end
---
+++ TramChallenge/TCTramStop.m
@@ -0,0 +1,13 @@
+//
+//  TCTramStop.m
+//  TramChallenge
+//
+//  Created by Stephen Sykes on 28/05/16.
+//  Copyright © 2016 Switchstep. All rights reserved.
+//
+
+#import "TCTramStop.h"
+
+@implementation TCTramStop
+
+@end
