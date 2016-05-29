ef4987d7c5ff21788df808ce3fea0f7d27af397d
Krister Kari <krister.kari@webtehdas.fi>
2016-05-29 14:03:58 +0300

    Configure leaderboard

--- TramChallenge/LeaderboardController.m
+++ TramChallenge/LeaderboardController.m
@@ -7,21 +7,40 @@
 //
 
 #import "LeaderboardController.h"
+#import "TCAPIAdaptor.h"
+
+@interface LeaderboardController () <UITableViewDelegate, UITableViewDataSource>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic) NSArray *leaderboard;
+@end
 
 @implementation LeaderboardController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.leaderboard = [[NSMutableArray alloc] init];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [[TCAPIAdaptor instance] getLeaderboardWithSuccess:^(NSArray *leaderboard) {
+        self.leaderboard = leaderboard;
+        [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+    } failure:^(NSError *error, NSInteger status, NSDictionary *info) {}];
+
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return [self.leaderboard count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"LeaderboardCell"];
-    cell.textLabel.text = @"Tram Challenge Team";
-    cell.detailTextLabel.text = @"00:59:59";
+    NSDictionary *team = [self.leaderboard objectAtIndex:indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@. %@", team[@"position"], team[@"name"]];
+    cell.detailTextLabel.text = team[@"elapsed_time"];
     return cell;
 }
 
